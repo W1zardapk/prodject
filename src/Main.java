8bf6428662fc0b3cd6dc7a269a91b54f12f48747
viktor <viktorkoer@gmail.com>
2024-03-03 20:08:06 +0300

    initial commit sout

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("Hello GitHub!");
         System.out.println("Hello Git!");
+        System.out.println("New Brunch");
     }
 }
